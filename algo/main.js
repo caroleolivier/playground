82bdf2983fe2ed1cc88c3ecb684f39de65c4a7ff
carole.olivier <carole.olivier@gmail.com>
2017-09-28 22:30:34 +0100

    Add function to convert to array

--- algo/main.js
+++ algo/main.js
@@ -3,13 +3,24 @@ function Cons(head, tail) {
     this.tail = tail;
 }
 
-Cons.toArray = toArray;
-
 function toArray(cons) {
-    debugger;
     if(cons) {
         let arr = [];
         return arr.concat(cons.head, toArray(cons.tail));
     }
     return [];
 }
+
+function fromArray(arr) {
+    if(arr == undefined || arr == null || arr.length < 1) {
+        return null;
+    }
+    let cons = new Cons(arr[arr.length-1], null);
+    for(let i = arr.length-2; i >= 0; i--) {
+        cons = new Cons(arr[i], cons);
+    }
+    return cons;
+}
+
+Cons.toArray = toArray;
+Cons.fromArray = fromArray;
--- algo/main.test.js
+++ algo/main.test.js
@@ -6,15 +6,42 @@ describe('toArray', () => {
         expect(actual).toEqual(expected);
     });
 
-    it('converts undefined cons to an array', () => {
+    it('converts undefined to an array', () => {
         let actual = Cons.toArray(undefined);
         let expected = [];
         expect(actual).toEqual(expected);
     });
 
-    it('converts null cons to an array', () => {
+    it('converts null to an array', () => {
         let actual = Cons.toArray(null);
         let expected = [];
         expect(actual).toEqual(expected);
+    });
+});
+
+describe('fromArray', () => {
+    it('converts an array to Cons', () => {
+        let arr = [1, 2, 3, 4];
+        let cons = Cons.fromArray(arr);
+        let expected = new Cons(1, new Cons(2, new Cons(3, new Cons(4, null))));
+        expect(cons).toEqual(expected);
+    });
+
+    it('converts undefined to null', () => {
+        let cons = Cons.fromArray(undefined);
+        let expected = null;
+        expect(cons).toEqual(expected);
+    });
+
+    it('converts null to null', () => {
+        let cons = Cons.fromArray(null);
+        let expected = null;
+        expect(cons).toEqual(expected);
+    });
+
+    it('converts empty array to null', () => {
+        let cons = Cons.fromArray([]);
+        let expected = null;
+        expect(cons).toEqual(expected);
     });
 });
